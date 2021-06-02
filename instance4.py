2a2ffe8b508bb90472aafeaced59557e9246a635
apinklover <61984863+apinklover@users.noreply.github.com>
2021-06-02 00:45:47 -0400

    Upload instances

---
+++ instance4.py
@@ -0,0 +1,23 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+import random
+import time
+
+time.sleep(5)
+
+url = "http://3.139.237.81/makotocoin"
+
+headers = CaseInsensitiveDict()
+headers["Content-Type"] = "application/json"
+num_hashes = 0
+
+for i in range(43200000, 44000000):
+    data = '{ "input": ' + str(i) + ' }'
+    resp = requests.post(url, headers=headers, data=data)
+    num_hashes += 1
+    print("Hash #" + str(num_hashes), data, resp)
+    if "200" in resp:
+        print("Correctly guessed the hash\nHash was " + data)
+        break
+
+input("Hash found. Press ENTER to exit.")
